5c75d8efd6f3c0835d1c24c16190836be9c4d4d1
damla yavan <damlayvn@icloud.com>
2024-05-05 00:22:19 +0300

    New Department

---
+++ MyMVCproject/MyMVCproject/Controllers/DepartmanController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using myfirstproject.Models.siniflar;
+using MyProject.Models.siniflar;
+
+namespace WebApplication2.Controllers
+{
+    public class DepartmanController : Controller
+    {
+
+
+        // GET: Departman
+        Context baglan=new Context();
+        public ActionResult Index()
+        {
+            var degerler = baglan.Departmans.ToList(); //departmanları getir
+            return View(degerler);
+        }
+
+
+        [HttpGet]
+        public ActionResult DepartmanEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DepartmanEkle(Departman departman)
+        {
+            baglan.Departmans.Add(departman);
+            baglan.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
