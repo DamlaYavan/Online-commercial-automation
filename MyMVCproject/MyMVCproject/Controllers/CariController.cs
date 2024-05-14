05c959f2726da1abdf8c0a0dcc6fdbd0f103ee9d
damla yavan <damlayvn@icloud.com>
2024-05-14 15:08:42 +0300

    Updates, additions, deletions and listings were made in the  Cariler section.

---
+++ MyMVCproject/MyMVCproject/Controllers/CariController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using myfirstproject.Models.siniflar;
+using  MyProject.Models.siniflar;
+
+namespace WebApplication2.Controllers
+{
+    public class CariController : Controller
+    {
+        // GET: Cari
+
+        Context Baglan = new Context();
+        public ActionResult Index()
+        {
+            var degerler = Baglan.Carilers.Where(x=>x.Durum==true).ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult CariEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CariEkle(Cariler cari)
+        {
+            if (!ModelState.IsValid)  //validasyon kontrolü
+            {
+                return View("CariEkle"); //validasyon işlemleri doğru değilse ekleme ekranına geri dön 
+            }
+            cari.Durum = true; // ilk eklendiğinde true yap 
+            Baglan.Carilers.Add(cari);
+            Baglan.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult CariSil(int id)
+        {
+            var newcari = Baglan.Carilers.Find(id);
+            newcari.Durum = false;
+            Baglan.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult CariGetir(int id) //Güncelleme işlemi için seçilen departmanı getiricek
+        {
+            var newCari = Baglan.Carilers.Find(id);
+            return View("CariGetir", newCari);
+
+        }
+
+        public ActionResult CariGuncelle(Cariler cari) //CariGetir Viewdan gelen departman
+        {
+            if (!ModelState.IsValid)  //validasyon kontrolü
+            {
+                return View("CariGetir"); //validasyon işlemleri doğru değilse güncelleme ekranına geri dön 
+            }
+            var newcari = Baglan.Carilers.Find(cari.CariId);
+            newcari.CariAd = cari.CariAd;
+            newcari.CariSoyad = cari.CariSoyad;
+            newcari.CariMail = cari.CariMail;
+            newcari.CariSehir = cari.CariSehir;
+            Baglan.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
