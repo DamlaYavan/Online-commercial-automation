e3da372fff6a6d385d811fb1c564fe1b546dfbab
damla yavan <damlayvn@icloud.com>
2024-05-19 17:36:43 +0300

    Faturalar

--- MyMVCproject/MyMVCproject/Models/siniflar/Faturalar.cs
+++ MyMVCproject/MyMVCproject/Models/siniflar/Faturalar.cs
@@ -22,7 +22,10 @@ namespace myfirstproject.Models.siniflar
         [Column(TypeName = "VarChar")]
         [StringLength(60)]
         public string VergiDairesi   { get; set; }
-        public DateTime Saat   { get; set; }
+
+        [Column(TypeName = "char")]
+        [StringLength(5)]
+        public string Saat   { get; set; }
 
         [Column(TypeName = "VarChar")]
         [StringLength(30)]
@@ -31,6 +34,8 @@ namespace myfirstproject.Models.siniflar
         [Column(TypeName = "VarChar")]
         [StringLength(30)]
         public string TeslimAlan   { get; set; }
+
+        public decimal Toplam { get; set; }
         public ICollection<FaturaKalem> FaturaKalems { get; set; } //bir gfaturanın birden fazla kalemi oalbilir
 
     }
