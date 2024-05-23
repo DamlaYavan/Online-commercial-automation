c0051d502e9fe3b5e4c34f8be356c43328709855
damla yavan <damlayvn@icloud.com>
2024-05-24 01:12:46 +0300

    Cari Panel add

---
+++ MyMVCproject/MyMVCproject/Controllers/CariPanel/CariPanelController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using myfirstproject.Models.siniflar;
+using MyProject.Models.siniflar;
+namespace WebApplication2.Controllers.CariPanel
+{
+    public class CariPanelController : Controller
+    {
+        // GET: CariPanel
+        Context baglan =new Context();
+        public ActionResult Index()  //Profilim bölümü
+        {
+            var mail = (string)Session["CariMail"];
+            var degerler = baglan.Carilers.FirstOrDefault(x => x.CariMail == mail);
+            ViewBag.carimail = mail;
+            return View(degerler);
+        }
+
+        public ActionResult Siparislerim()
+        {
+            var mail = (string)Session["CariMail"];
+            var id=baglan.Carilers.Where(x=>x.CariMail==mail.ToString()).
+                Select(y=>y.CariId).FirstOrDefault();
+            var degerler = baglan.SatisHarekets.Where(x => x.CariId == id).ToList();
+            return View(degerler);
+        }
+    }
+}
