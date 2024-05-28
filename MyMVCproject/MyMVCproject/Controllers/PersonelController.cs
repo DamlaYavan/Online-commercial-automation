45c6eef7ae5150f4700506ab534908fbd8df62b6
damla yavan <damlayvn@icloud.com>
2024-05-28 23:21:44 +0300

    editing

--- MyMVCproject/MyMVCproject/Controllers/PersonelController.cs
+++ MyMVCproject/MyMVCproject/Controllers/PersonelController.cs
@@ -66,18 +66,21 @@ namespace WebApplication2.Controllers
 
         public ActionResult PersonelGuncelle(Personel personel) //CariGetir Viewdan gelen departman
         {
-            if (Request.Files.Count > 0) // isteklerin arasında bi dosya varsa 
+            var newpersonel = baglan.Personels.Find(personel.PersonelId);
+
+            if (personel.PersonelGorsel!= null) // isteklerin arasında bi dosya varsa 
             {
                 string dosyaadi = Path.GetFileName(Request.Files[0].FileName); //dosyanın adını alıyoruz
                 string uzanti = Path.GetExtension(Request.Files[0].FileName);  //uzantıyı alıyoruz
                 string yol = "~/Image/" + dosyaadi + uzanti;
                 Request.Files[0].SaveAs(Server.MapPath(yol));
                 personel.PersonelGorsel = "/Image/" + dosyaadi + uzanti; //personelin görselini bu url yaptık.
+                newpersonel.PersonelGorsel = personel.PersonelGorsel;
+
             }
-            var newpersonel = baglan.Personels.Find(personel.PersonelId);
             newpersonel.PersonelAd = personel.PersonelAd;
             newpersonel.PersonelSoyad = personel.PersonelSoyad;
-            newpersonel.PersonelGorsel = personel.PersonelGorsel;
+            newpersonel.PersonelMail = personel.PersonelMail;
             newpersonel.Departmanid = personel.Departmanid;
             baglan.SaveChanges();
             return RedirectToAction("PersonelList");
